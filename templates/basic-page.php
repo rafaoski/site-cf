82efda8b1a44000e5c407702a78442b89360d2ab
Rafał Olszewicz <rafalolszewicz@gmail.com>
2019-08-11 15:16:05 +0200

    upd basic page -> schow children

--- templates/basic-page.php
+++ templates/basic-page.php
@@ -1,22 +1,21 @@
 <?php namespace ProcessWire; ?>
 
 <?php if (page()->hasChildren): ?>
-	<div id='body' class='body text-component' data-pw-append>
+<div id='main-content' class='body text-component' data-pw-append>
 
-	<h3><?= setting('more-topic') ?>:</h3>
+<h3><?= setting('more-topic') ?>:</h3>
 
-		<div class="body__content parent grid grid-gap-xs flex flex-center">
-			<?php foreach(page()->children as $item): ?>
-			<div class='component-card col-4@md text-component'>
-				<h3 class='text-md'><?= $item->title ?></h3>
-				<p class='text-sm'><?= $item->render('body', '_text-medium') ?></p>
-				<a class='btn btn--primary' href="<?= $item->url ?>">
-					<?= icon('chevrons-right', ['text_before' => setting('read-more')]) ?>
-				</a>
-			</div>
-			<?php endforeach; ?>
+	<div class="body__content parent grid grid-gap-xs flex flex-center">
+		<?php foreach(page()->children as $item): ?>
+		<div class='component-card col-4@md text-component'>
+			<h3 class='text-md'><?= $item->title ?></h3>
+			<p class='text-sm'><?= $item->render('body', '_text-medium') ?></p>
+			<a class='btn btn--primary' href="<?= $item->url ?>">
+				<?= icon('chevrons-right', ['text_before' => setting('read-more')]) ?>
+			</a>
 		</div>
+		<?php endforeach; ?>
 	</div>
-<?php endif ?>
 
-
+</div>
+<?php endif;
